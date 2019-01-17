3ba98406a92d1457f7fb1ae4d096f28d6ce6d8e2
Robbert van der Helm <mail@robbertvanderhelm.nl>
2019-01-17 13:16:17 +0100

    Format Static.hs

--- src/Static.hs
+++ src/Static.hs
@@ -19,9 +19,10 @@ data TypeEnvironment = TypeEnvironment
 type TExpression = TypeEnvironment -> (Expression, Type)
 
 type TArgument = TypeEnvironment -> (Argument, Type)
-type TLambda = TypeEnvironment -> (Lambda, Type)
-type TStatement = TypeEnvironment -> (Statement, TypeEnvironment)
 
+type TLambda = TypeEnvironment -> (Lambda, Type)
+
+type TStatement = TypeEnvironment -> (Statement, TypeEnvironment)
 
 check :: Hasql -> TypeEnvironment
 check = foldHasql checkAlgebra
@@ -78,23 +79,20 @@ check = foldHasql checkAlgebra
       case M.lookup s venv of
         Just t -> (s, t)
         Nothing -> error ("Variable " ++ s ++ " not defined")
-
     fExprOper expression1 operator expression2 env =
       case (operator, exprType) of
         (OperAdd, Just TypeInt) -> (Expr e1 OperAdd e2, TypeInt)
         (OperAdd, _) -> error "Arguments of addition were not both integers"
-
         (OperSubtract, Just TypeInt) -> (Expr e1 OperSubtract e2, TypeInt)
         (OperSubtract, _) ->
           error "Arguments of subtraction were not both integers"
-
         (OperMultiply, Just TypeInt) -> (Expr e1 OperMultiply e2, TypeInt)
         (OperMultiply, _) ->
           error "Arguments of multiplication were not both integers"
-
         (OperDivide, Just TypeInt) -> (Expr e1 OperDivide e2, TypeInt)
         (OperDivide, _) -> error "Arguments of division were not both integers"
-        (OperConcatenate, Just TypeString) -> (Expr e1 OperConcatenate e2, TypeString)
+        (OperConcatenate, Just TypeString) ->
+          (Expr e1 OperConcatenate e2, TypeString)
           -- XXX: This should not be a probem though!
         (OperConcatenate, _) ->
           error "Arguments of concatenation were not both strings"
@@ -102,8 +100,7 @@ check = foldHasql checkAlgebra
         (OperEquals, _) -> error "Arguments of (==) were not both booleans"
         (OperNotEquals, Just TypeBool) -> (Expr e1 OperNotEquals e2, TypeBool)
         (OperNotEquals, _) -> error "Arguments of (!=) were not both booleans"
-        (OperLesserThan, Just TypeBool) ->
-          (Expr e1 OperLesserThan e2, TypeBool)
+        (OperLesserThan, Just TypeBool) -> (Expr e1 OperLesserThan e2, TypeBool)
         (OperLesserThan, _) -> error "Arguments of (<) were not both booleans"
         (OperLesserEquals, Just TypeBool) ->
           (Expr e2 OperLesserEquals e1, TypeBool)
@@ -125,66 +122,82 @@ check = foldHasql checkAlgebra
             else Nothing
     operator1 :: Operator -> Operator
     operator1 = id
-
     lamda1 :: TExpression -> TLambda
-    lamda1 expr env = let (e, t) = expr env in (Lambda e, t)
-
+    lamda1 expr env =
+      let (e, t) = expr env
+       in (Lambda e, t)
     exprarg :: TExpression -> TArgument
-    exprarg expression env = let (e, t) = expression env in (ArgExpression e, t)
+    exprarg expression env =
+      let (e, t) = expression env
+       in (ArgExpression e, t)
     lamarg :: TLambda -> TArgument
-    lamarg lambda env = let (l, t) = lambda env in (ArgLambda l, t)
+    lamarg lambda env =
+      let (l, t) = lambda env
+       in (ArgLambda l, t)
     colarg :: Column -> TArgument
     colarg c env = (ArgColumn c, TypeString) -- String as placeholder "type"
     lsarg :: [String] -> TArgument
     lsarg asl env = (ArgStringList asl, TypeString) -- String as placeholder "type"
-
     operation1 :: Operation -> Operation
     operation1 = id
-
     operstat :: Operation -> [TArgument] -> TStatement
     --add column (NOT TESTED)
-    operstat (OperationAdd) [a1 , a2] env = do
+    operstat (OperationAdd) [a1, a2] env = do
       let TypeEnvironment {table = tenv, var = venv} = env
       let (Ident tableIdent) = extractIdent (fst (a1 env))
       let (Column n t1 mds) = extractColumn (fst (a2 env))
-      
       case M.lookup tableIdent tenv of
-          (Just table_env) -> do
-              case (M.lookup n table_env) of
-                (Just _) -> error ("Column "++n++" does already exist in Table "++tableIdent)
-                Nothing -> do
-                  let newTenv = M.insert n (t1, mds) table_env in 
-                    (FunctionCall OperationAdd (map (\a -> fst (a env)) [a1, a2]), TypeEnvironment {var = venv, table = M.adjust (\_ -> newTenv) tableIdent tenv})
-          Nothing -> error ("Table "++tableIdent++" does not exist") 
-
+        (Just table_env) -> do
+          case (M.lookup n table_env) of
+            (Just _) ->
+              error
+                ("Column " ++ n ++ " does already exist in Table " ++ tableIdent)
+            Nothing -> do
+              let newTenv = M.insert n (t1, mds) table_env
+               in ( FunctionCall OperationAdd (map (\a -> fst (a env)) [a1, a2])
+                  , TypeEnvironment
+                      { var = venv
+                      , table = M.adjust (\_ -> newTenv) tableIdent tenv
+                      })
+        Nothing -> error ("Table " ++ tableIdent ++ " does not exist")
     --split table
     operstat (OperationSplit) [a1, a2, a3] env = do
       let TypeEnvironment {table = tenv, var = venv} = env
       let (Ident tableIdent) = extractIdent (fst (a1 env))
       let newtablename = extractString (fst (a2 env))
-      let stringlist = extractStringList (fst (a3 env)) 
+      let stringlist = extractStringList (fst (a3 env))
       case M.lookup tableIdent tenv of
-          (Just table_env) -> do
-              case (M.lookup newtablename tenv) of
-                  Nothing -> do
-                      let newEnv = foldr (\column -> moveColumn tableIdent newtablename column) tenv stringlist in
-                        (FunctionCall OperationSplit (map (\a -> fst (a env)) [a1, a2, a3]), TypeEnvironment {var = venv, table = (M.insert newtablename M.empty newEnv)})                      
-                  Just t -> error ("Table "++newtablename++" does already exist")
-          Nothing -> error ("Table "++tableIdent++" does not exist") 
-
+        (Just table_env) -> do
+          case (M.lookup newtablename tenv) of
+            Nothing -> do
+              let newEnv =
+                    foldr
+                      (\column -> moveColumn tableIdent newtablename column)
+                      tenv
+                      stringlist
+               in ( FunctionCall
+                      OperationSplit
+                      (map (\a -> fst (a env)) [a1, a2, a3])
+                  , TypeEnvironment
+                      { var = venv
+                      , table = (M.insert newtablename M.empty newEnv)
+                      })
+            Just t -> error ("Table " ++ newtablename ++ " does already exist")
+        Nothing -> error ("Table " ++ tableIdent ++ " does not exist")
     moveColumn :: String -> String -> String -> TableEnv -> TableEnv
     moveColumn tfrom tto col tenv = do
       let (Just tablefrom) = M.lookup tfrom tenv
       let (Just tableto) = M.lookup tto tenv
       case M.lookup col tablefrom of
-        (Just (t, mds)) -> 
+        (Just (t, mds)) ->
           case M.lookup col tableto of
             Nothing -> do
-              let newEnv = M.adjust (\_ -> M.delete col tablefrom) tfrom tenv in 
-                M.adjust (\_ -> M.insert col (t, mds) tableto) tto newEnv                               
-            (Just _) -> error ("Column "++col++" does already exist in table "++tto)
-        Nothing -> error ("Column "++col++" does not exist in table "++tfrom)
-                  
+              let newEnv = M.adjust (\_ -> M.delete col tablefrom) tfrom tenv
+               in M.adjust (\_ -> M.insert col (t, mds) tableto) tto newEnv
+            (Just _) ->
+              error ("Column " ++ col ++ " does already exist in table " ++ tto)
+        Nothing ->
+          error ("Column " ++ col ++ " does not exist in table " ++ tfrom)
 
 extractIdent :: Argument -> Expression
 extractIdent (ArgExpression i@(Ident s)) = i
